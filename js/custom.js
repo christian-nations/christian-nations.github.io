72cabe04c2bdeb1fe398e881a33bb6415ff00f74
christian-nations <108565839+christian-nations@users.noreply.github.com>
2022-07-11 12:32:06 +0000

    Deploying to gh-pages from @ christian-nations/christian-nations.github.io@f24971b86d4ad5ce14e22c2aa49265cce917cda0 🚀

--- js/custom.js
+++ js/custom.js
@@ -125,3 +125,16 @@ function printDiv(divName) {
 function isDevice() {
     return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
 }
+//Resize
+// listen for page resize
+function resizeListener(dotnethelper) {
+    $(window).resize(() => {
+        let browserHeight = $(window).innerHeight();
+        let browserWidth = $(window).innerWidth();
+        dotnethelper.invokeMethodAsync('SetBrowserDimensions', browserWidth, browserHeight).then(() => {
+            // success, do nothing
+        }).catch(error => {
+            console.log("Error during browser resize: " + error);
+        });
+    });
+}
